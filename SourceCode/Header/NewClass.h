5e995c80568e7a9e0cc854cd11e94c23b975be3b
VictorLake <1657194622@qq.com>
2023-07-02 12:12:13 +0800

    design data structure

--- SourceCode/Header/NewClass.h
+++ SourceCode/Header/NewClass.h
@@ -7,21 +7,32 @@
 
 #include <CommonClass.h>
 
+#define ONE_FLOOR_CELL_NUMBER 10
+
+#define FIRE_HURT_COEFFICIENT
+#define SMOG_HURT_COEFFICIENT
+
+#define PERSON_NORMAL_SPEED
+#define PERSON_FULL_BLOOD 100
+
 struct Point
 {
     float X;
     float Y;
 };
 
-//角色类，管理角色的数据
+// 角色类，管理角色的数据
 class person : public CAnimateSprite
 {
 private:
     CAnimateSprite *m_pPerson;
 
-    Point m_PPersonPoi;      // 角色坐标
-    Point m_PPersonSpeed;    // 角色速度
+    Point m_PPersonPoi;   // 角色坐标
+    Point m_PPersonSpeed; // 角色速度
+    int m_iPersonBlood;   // 角色血量
+
     bool m_bPersonDirection; // 角色方向 1：朝左 2：朝右
+    bool m_bProp;            // 是否携带道具 1：携带 2：不携带
 
 public:
     person();
@@ -38,10 +49,20 @@ public:
 class floor : public CSprite
 {
 private:
-    CSprite *m_pFloor;
+    CSprite *m_pFloor; // 楼层元
 
     Point m_PFloorPoi; // 楼层坐标
     int m_iFloorNum;   // 楼层编号
+
+    CAnimateSprite *m_pFire; // 火焰元
+    bool m_bFireState[ONE_FLOOR_CELL_NUMBER]; // 单层火焰状态记录
+
+    CAnimateSprite *m_pSmog; // 烟雾元
+    bool m_bSmogState[ONE_FLOOR_CELL_NUMBER];  // 单层烟雾状态记录
+
+    prop *m_pPliers; // 钳子元
+
+
 
 public:
     floor();
@@ -52,9 +73,17 @@ public:
 
     // FloorUpdate: 更新楼层状态到引擎
     void FloorUpdate();
+
+    // FireDiffusion: 火焰扩散
+    void FireDiffusion();
+
+    // SmogDiffusion: 烟雾扩散
+    void SmogDiffusion();
+
+
 };
 
-//道具类，管理道具的数据
+// 道具类，管理道具的数据
 class prop : public CSprite
 {
 private:
@@ -69,7 +98,12 @@ public:
     // PropInit: 初始化道具
     void PropInit();
 
-    // PropUpdate:
+    // PropUpdate: 更新道具的状态到引擎
     void PropUpdate();
+
+    // IntoPropTable: 道具进入道具栏
+    void IntoPropTable();
+
+    //
 };
 #endif
