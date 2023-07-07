e5e7ad839a93e50f8ca416f3a4a139de69d9f6df
zsq0613 <717815630@qq.com>
2023-07-07 11:00:55 +0800

    07.07 --完成地图界面的制作--

--- SourceCode/Header/LessonX.h
+++ SourceCode/Header/LessonX.h
@@ -8,6 +8,10 @@
 #define _LESSON_X_H_
 //
 #include <Windows.h>
+#include "CommonClass.h"
+#include<mmsystem.h>
+#pragma comment(lib,"Winmm.lib") 
+
 
 /////////////////////////////////////////////////////////////////////////////////
 //
@@ -16,7 +20,16 @@ class	CGameMain
 {
 private:
 	int				m_iGameState;				// 游戏状态，0：结束或者等待开始；1：初始化；2：游戏进行中
-
+	int             m_jumpFlag;
+    CSprite         *Player1;
+	CSprite         *player2;
+	float           Player1_x1;
+    float           Player1_y1;
+	float           player2_x2;
+	float           player2_y2;
+	float           stair_x;
+	float           stair_y;
+	int             iPosX,iPosY;
 public:
 	CGameMain();            //构造函数
 	~CGameMain();           //析构函数
